8093616ec6270a2e71568414c1f2cef95190dac5
vgorin <vgrn.at@gmail.com>
2017-09-01 17:58:44 +0300

    the race condition described in https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729 is not an issue: approve should be treated as transfer approval and that's it

--- contracts/token/ERC20Token.sol
+++ contracts/token/ERC20Token.sol
@@ -90,7 +90,7 @@ contract ERC20Token is ERC20 {
 		//  allowance to zero by calling `approve(_spender, 0)` if it is not
 		//  already 0 to mitigate the race condition described here:
 		//  https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-		require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+		//require((_value == 0) || (allowed[msg.sender][_spender] == 0)); // bullshit: approve should be treated equal to transfer
 
 		// perform the operation
 		allowed[msg.sender][_spender] = _value;
