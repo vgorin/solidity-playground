34c1b80b4901d38b748ee7b3d548eb49370fbdf5
vgorin <vgrn.at@gmail.com>
2017-12-01 22:50:22 +0200

    improve deployment scripts

--- migrations/1.js
+++ migrations/1.js
@@ -1,6 +1,53 @@
 module.exports = async function(deployer, network, accounts) {
+	const Transfers = artifacts.require("./lib/Transfers");
+	const Accumulator = artifacts.require("./SharedAccumulator");
+	const Transfer = artifacts.require("./SharedTransfer");
 	const Token = artifacts.require("./token/ConfigurableERC20");
 	const Crowdsale = artifacts.require("./token/OpenCrowdsale");
+
+	await deployer.deploy(Transfers);
+	await deployer.link(Transfers, Accumulator);
+	await deployer.link(Transfers, Transfer);
+
+	await deployer.deploy(
+		Accumulator,
+		[
+			accounts[1],
+			accounts[2],
+			accounts[3]
+		],
+		[
+			6, 2, 1, // shares under 0.01 ETH
+			1, 6, 2, // shares between 0.01 and 0.02 ETH
+			2, 1, 6  // shares over 0.02 ETH
+		],
+		[
+			10000000000000000, // 0.01 ETH
+			20000000000000000, // 0.02 ETH
+			0
+		]
+	);
+	const accumulator = Accumulator.address;
+
+	await deployer.deploy(
+		Transfer,
+		[
+			accounts[1],
+			accounts[2],
+			accounts[3]
+		],
+		[
+			6, 2, 1,
+			1, 6, 2,
+			2, 1, 6
+		],
+		[
+			10000000000000000, // 0.01 ETH
+			20000000000000000, // 0.02 ETH
+			0
+		]
+	);
+	const transfer = Accumulator.address;
 
 	await deployer.deploy(
 		Token,
@@ -9,7 +56,6 @@ module.exports = async function(deployer, network, accounts) {
 		0,
 		400
 	);
-
 	const token = Token.address;
 
 	const now = new Date().getTime() / 1000 | 0;
@@ -22,12 +68,10 @@ module.exports = async function(deployer, network, accounts) {
 		100000000000000000, // 0.1 ETH
 		500000000000000000, // 0.5 ETH
 		0,
-		0x03cdA1F3DEeaE2de4C73cfC4B93d3A50D0419C24,
+		accounts[1],
 		token
 	);
-
 	const presale = Crowdsale.address;
-
 	await Token.at(token).approve(presale, 100);
 
 	await deployer.deploy(
@@ -38,14 +82,14 @@ module.exports = async function(deployer, network, accounts) {
 		500000000000000000, // 0.5 ETH
 		1000000000000000000, // 1.0 ETH
 		0,
-		0x03cdA1F3DEeaE2de4C73cfC4B93d3A50D0419C24,
+		accounts[2],
 		token
 	);
-
 	const crowdsale = Crowdsale.address;
-
 	await Token.at(token).approve(crowdsale, 100);
 
+	console.log("accumulator: " + accumulator);
+	console.log("transfer: " + transfer);
 	console.log("token: " + token);
 	console.log("presale: " + presale);
 	console.log("crowdsale: " + crowdsale);
--- truffle.js
+++ truffle.js
@@ -9,16 +9,9 @@ module.exports = {
 		},
 		testrpc: {
 			host: "localhost",
-			port: 8550,
+			port: 8546,
 			network_id: "*", // Match any network id
 			gas: 2000000
-		},
-		coverage: {
-			host: "localhost",
-			network_id: "*",
-			port: 8551,         // <-- If you change this, also set the port option in .solcover.js.
-			gas: 0xfffffffffff, // <-- Use this high gas value
-			gasPrice: 0x01      // <-- Use this low gas price
 		}
 	}
 };
