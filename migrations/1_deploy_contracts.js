806a67c55d4a2c77c8bc9bb30284bf6453d6ec66
vgorin <vgrn.at@gmail.com>
2018-01-18 00:45:29 +0200

    deployment script refactoring

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -60,63 +60,82 @@ module.exports = async function(deployer, network, accounts) {
 	const transfer = Accumulator.address;
 */
 
+	const totalSupply = 11788618;
+
+	const offset0 = new Date('2018-01-18T00:00').getTime() / 1000 | 0;
+	const length0 = 3600;
+	const price0 = 2000000000000000; // 2 finney (0.002 ether)
+	const soft0 = 0;
+	const hard0 = 2666666000000000000000; // 2 666.666 ether
+	const tokens0 = 1333333;
+	const offset1 = new Date('2018-01-18T00:00').getTime() / 1000 | 0;
+	const length1 = 3600;
+	const price1 = 3000000000000000; // 3 finney (0.003 ether)
+	const soft1 = 0;
+	const hard1 = 20000001000000000000000; // 20 000.001 ether
+	const tokens1 = 6666667;
+	const offset2 = new Date('2018-01-18T00:00').getTime() / 1000 | 0;
+	const length2 = 3600;
+	const price2 = 4000000000000000; // 4 finney (0.004 ether)
+	const soft2 = 0;
+	const hard2 = 6666668000000000000000; // 6 666.668 ether
+	const tokens2 = 1666667;
+
+
+	const digits = 0;
+	const k = Math.pow(10, digits);
+	const p = 10000; // should be 1 for production
+
 	await deployer.deploy(
 		Token,
-		"TS1",
-		"Test1",
-		0,
-		11788618
+		"TS3",
+		"Test3",
+		digits,
+		totalSupply * k
 	);
 	const token = Token.address;
-
-	const offset0 = 1516216800;
-	const offset1 = 1516217400;
-	const offset2 = 1516218600;
-	const length0 = 600;
-	const length1 = 600;
-	const length2 = 600;
 
 	await deployer.deploy(
 		Crowdsale,
 		offset0,
-		600,
-		2000000000000000 / 10000, // 2 finney (0.002 ether)
-		0,
-		2666666000000000000000 / 10000, // 2 666.666 ether
+		length0,
+		price0 / p,
+		soft0,
+		hard0 / p,
 		0,
 		accounts[0],
 		token
 	);
 	const presale = Crowdsale.address;
-	await Token.at(token).approve(presale, 1333333);
+	await Token.at(token).approve(presale, tokens0 * k);
 
 	await deployer.deploy(
 		Crowdsale,
 		offset1,
-		600,
-		3000000000000000 / 10000, // 3 finney (0.003 ether)
-		0,
-		20000001000000000000000 / 10000, // 20 000.001 ether
+		length1,
+		price1 / p,
+		soft1,
+		hard1 / p,
 		0,
 		accounts[0],
 		token
 	);
 	const crowdsale1 = Crowdsale.address;
-	await Token.at(token).approve(crowdsale1, 6666667);
+	await Token.at(token).approve(crowdsale1, tokens1 * k);
 
 	await deployer.deploy(
 		Crowdsale,
 		offset2,
-		600,
-		4000000000000000 / 10000, // 3 finney (0.003 ether)
-		0,
-		6666668000000000000000 / 10000, // 6 666.668 ether
+		length2,
+		price2 / p,
+		soft2,
+		hard2 / p,
 		0,
 		accounts[0],
 		token
 	);
 	const crowdsale2 = Crowdsale.address;
-	await Token.at(token).approve(crowdsale2, 1666667);
+	await Token.at(token).approve(crowdsale2, tokens2 * k);
 
 
 	const now = new Date().getTime() / 1000 | 0;
