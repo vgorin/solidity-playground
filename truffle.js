729f544826fc77b668f799fa93b6ccdb1fde0541
vgorin <vgrn.at@gmail.com>
2018-01-18 00:47:56 +0200

    update config

--- truffle.js
+++ truffle.js
@@ -3,10 +3,13 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
 	networks: {
 		development: {
-			provider: new HDWalletProvider("***mnemonic***", "https://ropsten.infura.io/***key***"),
-			network_id: "3", // Match any network id
-			gas: 4000000,
-			gasPrice: 20000000000 // 20 GWei
+			provider: new HDWalletProvider(
+				"***",
+				"https://ropsten.infura.io/***"
+			),
+			network_id: "3", // Match only Ropsten
+			gas: 2000000,
+			gasPrice: 50000000000 // 50 GWei
 		},
 		testrpc: {
 			host: "localhost",
